eeaaecdebadc04552e33856e7d140709dd44689a
A12134 <35416592+A12134@users.noreply.github.com>
2019-09-03 17:09:38 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## README
+
+# Usage:
+
+get training data in lists:
+```
+from fileReader import trainData
+t = trainData()
+label, data = t.getLabelsAndrawData
+
+```
+
+samples for training SVM model in file SVMmodel.py
---
+++ featureExtractor.py
@@ -0,0 +1,419 @@
+import re
+from nltk import ngrams
+from nltk import TweetTokenizer
+import emot
+from w2v_processing import w2vAndGramsConverter
+import operator
+import string
+
+class extractor:
+    def __init__(self):
+        self.vocab = None
+        self.firstVocab = dict()
+        self.firstCount = 0
+        self.secondVocab = dict()
+        self.secondCount = 0
+        self.thirdVocab = dict()
+        self.thirdCount = 0
+
+        self.ngramVocab = None
+        self.firstNgram = dict()
+        self.fnc = 0
+        self.secondNgram = dict()
+        self.snc = 0
+
+        self.gramrdVocab = None
+
+    def isRetweet(self, line):
+        if re.search(r"[R|r][T|t]:?\s@handle:?\s?", line):
+            return 1.
+        return 0.
+
+    def removeCommonUsedWord(self, dic):
+        ignoreToken = [
+            "to",
+            "a",
+            'and',
+            "i",
+            "for",
+            "of",
+            "in",
+            "is",
+            "on",
+            "you",
+            "my",
+            "rt",
+            "-",
+            "at",
+            "handle",
+            "the",
+            "im"
+        ]
+
+        for t in ignoreToken:
+            if dic.get(t) is not None and len(t) <= 2:
+                del dic[t]
+
+        return dic
+
+    def removePuncu(self, line):
+        s = line.translate(str.maketrans('','',string.punctuation))
+        return s
+
+    def lineNormalization(self, line):
+        tknzr = TweetTokenizer()
+        norm = w2vAndGramsConverter()
+        line = norm.normalizeSentence(line)
+        line = self.removePuncu(line)
+        line = line.lower()
+        tokens = tknzr.tokenize(line)
+        return tokens
+
+    def highFrequencyTokens(self, label, lines):
+        currentUsr = label[0]
+        gramrd = []
+        userrdGram = dict()
+        existrdGram = dict()
+        gram = []
+        userNgram = dict()
+        existGram = dict()
+        vocab = []
+        existvocab = dict()
+        userToken = dict()
+        for i in range(label.__len__()):
+            print("calculating high frequent word features ====>" + str((i+1)*100/label.__len__()) + "%")
+            if label[i] is currentUsr:
+                line = lines[i]
+                tokens = self.lineNormalization(line)
+                grams = ngrams(tokens, 2)
+                rdgr = ngrams(tokens, 3)
+
+                for rd in rdgr:
+                    if userrdGram.get(rd) is None:
+                        userrdGram[rd] = 1
+                    else:
+                        userrdGram[rd] += 1
+
+                for g in grams:
+                    if userNgram.get(g) is None:
+                        userNgram[g] = 1
+                    else:
+                        userNgram[g] += 1
+
+                for t in tokens:
+                    if userToken.get(t) is None:
+                        userToken[t] = 1
+                    else:
+                        userToken[t] += 1
+            else:
+                userToken = self.removeCommonUsedWord(userToken)
+                sortList = sorted(userToken.items(), key=operator.itemgetter(1), reverse=True)
+                gramSortList = sorted(userNgram.items(), key=operator.itemgetter(1), reverse=True)
+                rdSordList = sorted(userrdGram.items(), key=operator.itemgetter(1), reverse=True)
+
+                # fixed token position vector
+                if self.firstVocab.get(sortList[0][0]) is None:
+                    self.firstVocab[sortList[0][0]] = self.firstCount
+                    self.firstCount += 1
+                if self.secondVocab.get(sortList[1][0]) is None:
+                    self.secondVocab[sortList[1][0]] = self.secondCount
+                    self.secondCount += 1
+                if self.thirdVocab.get(sortList[2][0]) is None:
+                    self.thirdVocab[sortList[2][0]] = self.thirdCount
+                    self.thirdCount += 1
+
+                if self.firstNgram.get(gramSortList[0][0]) is None:
+                    self.firstNgram[gramSortList[0][0]] = self.fnc
+                    self.fnc += 1
+                if self.secondNgram.get(gramSortList[1][0]) is None:
+                    self.secondNgram[gramSortList[1][0]] = self.snc
+                    self.snc += 1
+
+                if existrdGram.get(rdSordList[0][0]) is None:
+                    gramrd.append(rdSordList[0][0])
+                    existrdGram[rdSordList[0][0]] = gramrd.__len__()-1
+
+                # not used
+                if existGram.get(gramSortList[0][0]) is None:
+                    gram.append(gramSortList[0][0])
+                    existGram[gramSortList[0][0]] = gram.__len__()-1
+                if existGram.get(gramSortList[1][0]) is None:
+                    gram.append(gramSortList[1][0])
+                    existGram[gramSortList[1][0]] = gram.__len__()-1
+
+                # store top 2 user frequent words, not using
+                if existvocab.get(sortList[0][0]) is None:
+                    vocab.append(sortList[0][0])
+                    existvocab[sortList[0][0]] = vocab.__len__()-1
+                if existvocab.get(sortList[1][0]) is None:
+                    vocab.append(sortList[1][0])
+                    existvocab[sortList[1][0]] = vocab.__len__()-1
+
+                userrdGram = dict()
+                userNgram = dict()
+                userToken = dict()
+                currentUsr = label[i]
+
+                line = lines[i]
+                tokens = self.lineNormalization(line)
+                grams = ngrams(tokens, 2)
+                rdgr = ngrams(tokens, 3)
+
+                for rd in rdgr:
+                    if userrdGram.get(rd) is None:
+                        userrdGram[rd] = 1
+                    else:
+                        userrdGram[rd] += 1
+
+                for g in grams:
+                    if userNgram.get(g) is None:
+                        userNgram[g] = 1
+                    else:
+                        userNgram[g] += 1
+
+                for t in tokens:
+                    if userToken.get(t) is None:
+                        userToken[t] = 1
+                    else:
+                        userToken[t] += 1
+        self.vocab = existvocab
+        self.ngramVocab = existGram
+        self.gramrdVocab = existrdGram
+
+    def mentionWordRatio(self, line):
+        l = re.findall(r"(?<![R|r][T|t]\s)@handle", line)
+        w = self.totalWord(line)
+        return l.__len__() / w
+
+    def totalWord(self, line):
+        return line.split(" ").__len__()
+
+    def numOfURL(self, line):
+        l = re.findall("http", line)
+        return l.__len__()
+
+    def hashtagWordRatio(self, line):
+        l = re.findall(r"(?<!&)#", line)
+        w = self.totalWord(line)
+        return l.__len__() / w
+
+    def numOfMoney(self, line):
+        l = re.findall(r"\$[1-9][0-9]*", line)
+        return l.__len__()
+
+    def lineToVector(self, line):
+        return [
+            self.isRetweet(line),
+            self.mentionWordRatio(line),
+            self.totalWord(line),
+            self.hashtagWordRatio(line),
+            self.numOfMoney(line)
+        ]
+
+    def str(self, vec):
+        s = ""
+        for x in vec:
+            s += str(x) + ','
+        s = s[:-1]
+        return s
+
+    def batchToVector(self, d, usr_flag, save=False, file_name="data"):
+        print("convert raw data to vector.....")
+        if save:
+            print("saving enabled, writing to file: " + file_name)
+            save_file = open(file_name, 'w', encoding='utf-8')
+            if usr_flag:
+                for k in d.keys():
+                    for l in d[k]:
+                        save_file.write(k + '\t' + self.str(self.lineToVector(l)) + '\n')
+            else:
+                for l in d:
+                    save_file.write(self.str(self.lineToVector(l)) + '\n')
+
+            save_file.close()
+            print("saving complete!")
+
+        else:
+            if usr_flag:
+                new_d = d
+                for k in d.keys():
+                    for l in d[k]:
+                        new_d[k] = self.lineToVector(l)
+
+                return new_d
+
+            else:
+                d_arr = []
+                for l in d:
+                    d_arr.append(self.lineToVector(l))
+
+                return d_arr
+
+    """
+    ===============================
+            Binary/n Features
+    +++++++++++++++++++++++++++++++
+    """
+
+    def hasEmoji(self, line):
+        result = emot.emoticons(line)
+        if result.__len__() > 0:
+            return 1
+        return 0
+
+    # 1: less than 10 tokens(include)
+    # 2: less than 20 tokens(include)
+    # 3: less than 30 tokens(include)
+    # 4: less than 40 tokens(include)
+    # 5: more than 40 tokens(exclude)
+    def tweetLength(self, line):
+        # normalize the line
+        w2vLib = w2vAndGramsConverter()
+        line = w2vLib.normalizeSentence(line)
+
+        # tokenize sentence
+        tnz = TweetTokenizer()
+        tokens = tnz.tokenize(line)
+
+        if tokens.__len__() <= 10:
+            return 1
+        elif tokens.__len__() <= 20:
+            return 2
+        elif tokens.__len__() <= 30:
+            return 3
+        elif tokens.__len__() <= 40:
+            return 4
+        else:
+            return 5
+
+    # 0: no URL
+    # 1: 1 URL
+    # 2: 2 URL
+    # 3: 3 URL
+    # 4: more than 3 URL
+    def getURLFeature(self, line):
+        return self.numOfURL(line)
+
+    def wordUseFeature(self, line):
+        tokens = self.lineNormalization(line)
+        count = 0
+        feature = [-1,-1,-1,-1,-1,-1]
+
+        gr = ngrams(tokens, 2)
+        grrd = ngrams(tokens, 3)
+
+        for rd in grrd:
+            if self.gramrdVocab.get(rd) is not None:
+                feature[5] = self.gramrdVocab[rd]
+                break
+
+        for g in gr:
+            if self.firstNgram.get(g) is not None:
+                feature[0] = self.firstNgram[g]
+            elif self.secondNgram.get(g) is not None:
+                feature[1] = self.secondNgram[g]
+
+
+        for t in tokens:
+
+            if self.firstVocab.get(t) is not None:
+                feature[2] = self.firstVocab.get(t)
+            elif self.secondVocab.get(t) is not None:
+                feature[3] = self.secondVocab.get(t)
+            elif self.thirdVocab.get(t) is not None:
+                feature[4] = self.thirdVocab.get(t)
+
+        return feature
+
+    def isRT(self, line):
+        return self.isRetweet(line)
+
+    def containMoney(self, line):
+        if self.numOfMoney(line) > 0:
+            return 1
+        return 0
+
+    def hasCaptialWord(self, line):
+        return re.findall(r"\s[A-Z]{2,}", line).__len__()
+        """
+        if re.search(r"\s[A-Z]{2,}", line):
+            return 1
+        return 0
+        """
+
+    def hasNowPlaying(self, line):
+        if re.search(r"Now playing:\s", line):
+            return 1
+        return 0
+
+    def useOfPuncs(self, line):
+        if re.search(r".{2,}", line) and re.search(r"!{2,}", line):
+            return 3
+        elif re.search(r".{2,}", line):
+            return 2
+        elif re.search(r"!{2,}", line):
+            return 1
+        return 0
+
+    def hasNum(self, line):
+
+        if re.search(r"\s[0-9]*\s", line):
+            return 1
+        return 0
+
+    def hasMention(self, line):
+        return re.findall(r"(?<![R|r][T|t]\s)@handle", line).__len__()
+        """
+        if re.search(r"(?<![R|r][T|t]\s)@handle", line):
+            return 1
+        return 0
+        """
+
+    def hasRepeatLetters(self, line):
+        if re.search(r"\s[a-zA-Z]*([a-zA-Z])\1{1}[a-zA-Z]*\s", line):
+            return 1
+        return 0
+
+    def hasHash(self, line):
+        return re.findall(r"#\w+", line).__len__()
+        """
+        if re.search(r"#\w+", line):
+            return 1
+        return 0
+        """
+
+    def lineToFixFeatureVec(self, line):
+        return [
+            self.tweetLength(line),
+            self.isRT(line),
+            self.getURLFeature(line),
+            self.hasMention(line),
+            self.hasRepeatLetters(line),
+            self.hasNum(line),
+            self.containMoney(line),
+            self.useOfPuncs(line),
+            self.hasCaptialWord(line),
+            self.hasEmoji(line),
+            self.hasHash(line),
+        ] + self.wordUseFeature(line)
+
+    def batchProduceFixFeatureVec(self, lines):
+        print("transfering rawdata into vectors.....")
+        retArr = []
+        for i in range(lines.__len__()):
+            print("transfering rawdata into vectors ======>" + str((i+1)*100/lines.__len__()))
+            retArr.append(self.lineToFixFeatureVec(lines[i]))
+
+        print("transfer complete!")
+        return retArr
+
+    # 0: no capital of word
+    # 1: 50% of words are start with Capital
+    def COWvalue(self, line):
+        l = re.findall(r'[A-Z][a-z]+', line)
+        w = re.findall(r'[a-zA-Z]+\s', line)
+        v = l.__len__() / w.__len__()
+        if v > 0.5:
+            return 1
+        else:
+            return 0
