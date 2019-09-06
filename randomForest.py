6c490c50a86f4b93a5424839e610e30003600774
A12134 <ybandyawes@gmail.com>
2019-09-06 21:40:09 +1000

    fuk mem!

--- randomForest.py
+++ randomForest.py
@@ -11,28 +11,40 @@ from sys import getsizeof
 import pickle
 import atexit
 import gc
+import ctypes
 
-td = trainData(threshold=50)
-testd = testData()
-label,raw = td.getLabelsAndrawData()
 
-#process data
-ext = extractor()
+def malloc_trim():
+    ctypes.CDLL('libc.so.6').malloc_trim(0)
 
-ext.loadCacheFile()
-ext.highFrequencyTokens(label, raw)
-ext.extractEmoji(raw)
-ext.extractHashTags(raw)
-data = ext.batchProduceFixFeatureVec(raw)
-tdata = ext.batchProduceFixFeatureVec(testd.getAllTweets())
-td.unloadData()
-#ext.saveCacheFiles()
-ext.unloadExt()
-del ext
-del raw
+def scope():
+    td = trainData(threshold=50)
+    testd = testData()
+    label,raw = td.getLabelsAndrawData()
 
-print(getsizeof(data))
-print("clean trash...")
+    #process data
+    ext = extractor()
+
+    ext.loadCacheFile()
+    ext.highFrequencyTokens(label, raw)
+    ext.extractEmoji(raw)
+    ext.extractHashTags(raw)
+    data = ext.batchProduceFixFeatureVec(raw)
+    tdata = ext.batchProduceFixFeatureVec(testd.getAllTweets())
+    td.unloadData()
+    #ext.saveCacheFiles()
+    #ext.unloadExt()
+    del ext
+    del raw
+
+
+    print(getsizeof(data))
+    print("clean trash...")
+
+    malloc_trim()
+
+
+    return label, data, tdata
 
 #X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=0)
 
@@ -41,6 +53,8 @@ print("clean trash...")
 #gnb = GaussianNB()
 #gnb.fit(X_train, y_train)
 #y_pred = gnb.predict(X_test)
+
+label, data, tdata = scope()
 
 # train model
 regressor = RandomForestClassifier(n_estimators=20, criterion='entropy', verbose=10, n_jobs=2)
